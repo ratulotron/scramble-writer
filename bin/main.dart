c80184d3911f786339b044861be3dad495f72a49
Ratul Minhaz <m@ratul.xyz>
2019-01-06 01:58:22 +0600

    Initialized console app project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:scramble_writer/scramble_writer.dart' as scramble_writer;
+
+main(List<String> arguments) {
+  print('Hello world: ${scramble_writer.calculate()}!');
+}
---
+++ test/scramble_writer_test.dart
@@ -0,0 +1,8 @@
+import 'package:scramble_writer/scramble_writer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
